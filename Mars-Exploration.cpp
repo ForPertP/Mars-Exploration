e12cb2881549668024fb6655769a82eb46bf0391
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-09-23 02:46:09 +0900

    Update Mars-Exploration.cpp

--- Mars-Exploration.cpp
+++ Mars-Exploration.cpp
@@ -1,1 +1,13 @@
-
+int marsExploration(string s)
+{
+    int count = 0;
+    
+    for (int i = 0; i < s.size(); i += 3 )
+    {
+        if( s[i]   != 'S' ) count++;
+        if( s[i+1] != 'O' ) count++;
+        if( s[i+2] != 'S' ) count++;    
+    }
+    
+    return count;
+}
