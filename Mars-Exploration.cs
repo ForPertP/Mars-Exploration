a58cd2012ae17e53eb945a7d77e1511b048f2eb0
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-02-27 05:35:20 +0900

    Update Mars-Exploration.cs

--- Mars-Exploration.cs
+++ Mars-Exploration.cs
@@ -22,7 +22,23 @@ class Result
      * The function accepts STRING s as parameter.
      */
 
-    public static int marsExploration(string s)
+
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
     {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int result = Result.marsExploration(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
